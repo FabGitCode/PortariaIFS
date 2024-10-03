6b1a3586df2d4d2bc027bfc88ea15419f99376b8
FabGitCode <fabianofilho998@gmail.com>
2024-10-03 17:56:36 -0300

    Update Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -1,1 +1,42 @@
+// Classe Estudante
+class Estudante {
+    private String nome;
+    private String cpf;
+    private String email;
+    private int matricula;
+    private String emailResponsavel;
+    private boolean autorizacaoPais;
 
+    public Estudante(String nome, String cpf, String email, int matricula, String emailResponsavel, boolean autorizacaoPais) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.matricula = matricula;
+        this.emailResponsavel = emailResponsavel;
+        this.autorizacaoPais = autorizacaoPais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getEmailResponsavel() {
+        return emailResponsavel;
+    }
+
+    public boolean isAutorizacaoPais() {
+        return autorizacaoPais;
+    }
+}
