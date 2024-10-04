2e09dda1537bc818b4fd5399ffd9e2e1c1fdf3c5
FabGitCode <fabianofilho998@gmail.com>
2024-10-03 22:51:19 -0300

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Estudante> estudantes = new ArrayList<>();
+        List<Curso> cursos = new ArrayList<>();
+        List<Turma> turmas = new ArrayList<>();
+
+        Menu menu = new Menu(estudantes, cursos, turmas);
+        menu.exibirMenu();
+    }
+}
